7aaa57f83ffe03074a6f7f9301dc590577740624
Jacques Spykerman <jacwil747@gmail.com>
2025-06-26 13:25:27 +0000

    Log still not displaying in header

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,16 +1,14 @@
 import Link from "next/link";
-import Image from "next/image";
 
 export function Logo() {
   return (
     <Link href="/" className="flex items-center group" prefetch={false}>
-      <Image
+      <img
         src="https://storage.googleapis.com/source-www-uploads-prod/images/655883216.png"
         alt="Blinkogies Logo"
         width={176}
         height={174}
         className="h-12 w-auto"
-        unoptimized
       />
     </Link>
   );
