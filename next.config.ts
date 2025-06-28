03a9a796c684d4965aa30d0809e2b1f2427d01ea
Jacques Spykerman <jacwil747@gmail.com>
2025-06-28 11:04:09 +0000

    still no install prompt

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,17 @@
+
 import type {NextConfig} from 'next';
 import withPWAInit from '@ducanh2912/next-pwa';
 
 const withPWA = withPWAInit({
   dest: 'public',
+  disable: false,
   register: true,
   skipWaiting: true,
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  fallbacks: {
+    document: '/offline',
+  },
 });
 
 const nextConfig: NextConfig = {
