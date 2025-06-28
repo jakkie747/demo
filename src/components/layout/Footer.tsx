21ce1d934b6d187fe63545d123b2d8f9ad58353c
Jacques Spykerman <jacwil747@gmail.com>
2025-06-28 10:04:29 +0000

    Change the copyright info to read 2025 Blinkogies App

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -11,7 +11,7 @@ export function Footer() {
       <div className="container mx-auto flex flex-col items-center justify-between gap-6 py-10 sm:flex-row">
         <Logo />
         <p className="text-sm text-muted-foreground">
-          {t('copyright', { year: new Date().getFullYear().toString() })}
+          © 2025 Blinkogies App
         </p>
         <div className="flex items-center gap-4">
           <Link
