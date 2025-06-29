c701abcf40cef3bb36a646e619c8d34270e69e55
Jacques Spykerman <jacwil747@gmail.com>
2025-06-29 13:04:50 +0000

    Build artifacts Log viewer toolbar starting build "8d7eb9db-b28f-44fe-89

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,4 +1,3 @@
-
 "use client"
 
 import * as React from "react"
@@ -55,8 +54,8 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: (props) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: (props) => <ChevronRight className="h-4 w-4" />,
+        IconLeft: ChevronLeft,
+        IconRight: ChevronRight,
       }}
       {...props}
     />
