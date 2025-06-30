c43ad76c931bb429d007c095878ed3f066429b69
Jacques Spykerman <jacwil747@gmail.com>
2025-06-30 05:13:35 +0000

    centre the text on the homepage at the moment it is all left aligned

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -101,7 +101,7 @@ export default function Home() {
       <section className="w-full py-12 md:py-24 lg:py-32 bg-primary/10">
         <div className="container px-4 md:px-6">
           <div className="grid gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px]">
-            <div className="flex flex-col justify-center space-y-4">
+            <div className="flex flex-col items-center justify-center space-y-4 text-center">
               <div className="space-y-2">
                 <h1 className="font-headline text-4xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none text-primary">
                   {t("welcome")}
