e04210f79c66decb7fe2e4253ad49f2b2e5ad342
Jacques Spykerman <jacwil747@gmail.com>
2025-07-03 05:42:03 +0000

    The buttons on registered children still visible on mobile though

--- src/app/admin/dashboard/children/page.tsx
+++ src/app/admin/dashboard/children/page.tsx
@@ -154,11 +154,9 @@ export default function ChildrenPage() {
 
   return (
     <div className="py-6 space-y-6">
-       <div className="flex justify-between items-center">
-        <h2 className="text-3xl font-bold tracking-tight">
+       <h2 className="text-3xl font-bold tracking-tight">
             {t('registeredChildrenTitle')}
-        </h2>
-      </div>
+       </h2>
       <div className="w-full overflow-x-auto">
         <Card>
             <CardContent className="p-0">
