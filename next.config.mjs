0ade12537c60602f7336138cce0c694a8d9f1264
Jacques Spykerman <jacwil747@gmail.com>
2025-07-04 18:50:13 +0000

    ERROR: build step 3 "gcr.io/k8s-skaffold/pack" failed: step exited with

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-      },
-      {
-        protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-      },
-    ],
+  webpack: (config) => {
+    // This is a workaround for a build error related to the 'handlebars' library,
+    // a dependency of Genkit. Handlebars attempts to use Node.js's 'fs' module,
+    // which is not available in the browser and causes the client-side build to fail.
+    // By setting `config.resolve.fallback.fs = false`, we instruct Webpack to
+    // exclude this module from the client bundle, resolving the build error.
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+    };
+
+    return config;
   },
 };
 
